a25d2e79c8d9653c2a509dc27f958453925911d5
DiegoAlvarez44 <diegooalvarez@hotmail.com>
2022-10-30 20:47:29 +0100

    a

--- Asistencia_Octubre.py
+++ Asistencia_Octubre.py
@@ -137,3 +137,61 @@ print(f'Este es el telefono de: {persona1.nombre} {persona1.telefono}') # Hemos 
 # print(persona2.telefono) el objeto persona2 no tiene este atributo, da error
 persona3 = Persona('Rogelio', 'Romero', 35789456, 22, 'Teléfono', '2614445557', 'Calle Lopez', 823, 'Manzana', 77, 'Casa', 18, Altura=1.83, Peso=105, CFavorito='Azul', Auto='Citroen', Modelo=2021)
 persona3.mostrar_detalle()
+
+#-------------Alumna: Diego Alvarez-----------------
+
+class Persona(): # Esta clase hereda de la clase Object
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    
+    @property
+    def edad(self):
+        return self._edad
+    
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
+    def __str__(self): # Override = sobreescribir
+        return f'Persona:[ Nombre: {self._nombre}, Edad: {self._edad}]'
+
+class Empleado(Persona): # La clase empleado es hija de la clase persona
+    def __init__(self, nombre, edad, sueldo):
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+
+    @property
+    def sueldo(self):
+        return self._sueldo
+    
+    @sueldo.setter
+    def sueldo(self, sueldo):
+        self._sueldo = sueldo
+
+    
+empleado1 = Empleado('Diego', 24, 75000)
+print(empleado1.nombre)
+print(empleado1.edad)
+print(empleado1.sueldo)
+# Tarea: encapssular los atributos y agregar los metodos gatters and setters
+# Crear otro objeto, pasar los datos para nombre, edad y sueldo
+# Mostrar estos datos, luego modificar y mostrar nuevamente
+
+empleado2 = Empleado('Fernando', 35, 85000)
+print(empleado2.nombre)
+print(empleado2.edad)
+print(empleado2.sueldo)
+
+empleado3 = Empleado('Arnolito', 44, 90000)
+print(empleado3.nombre)
+print(empleado3.edad)
+print(empleado3.sueldo)
